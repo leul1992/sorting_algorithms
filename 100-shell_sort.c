0d3f2e3f530c8d270c5e1a8650b538d3fbe64f84
Leulseged <leulbekele191@gmail.com>
2022-07-19 20:48:06 +0300

    sort array using shellsort

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort- sort array
+ * @array: the pointer to the array
+ * @size: size of the array
+ * Return: nothing
+ */
+ void shell_sort(int *array, size_t size)
+ {
+    int temp;
+    size_t i, j, gap = 1;
+
+    while (gap <= size / 3)
+    {
+        gap = (gap * 3) + 1;
+    }
+    while (gap > 0)
+    {
+        for (i = gap; i < size; i++)
+        {
+            temp = array[i];
+            for (j = i; j > gap - 1 && array[j - gap] >= temp ; j = j - gap)
+            {
+                array[j] = array[j - gap];
+            }
+            array[j] = temp;
+        }
+        print_array(array, size);
+        gap = (gap - 1) / 3;
+    }
+ }
