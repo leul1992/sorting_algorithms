20b583c6e5912d2556d1772d894533201b96ef06
Lidya188 <lidyagenene@gmail.com>
2022-07-19 16:41:02 -0400

    all

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,61 +1,118 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "sort.h"
 
+void swap(listint_t **head, listint_t *node1, listint_t *node2);
 /**
- * len_list - returns the length of a linked list
- * @h: pointer to the list
+ * insertion_sort_list - sorts a doubly linked list with
+ * the insertion sort algorithm
  *
- * Return: length of list
- */
-int len_list(listint_t *h)
-{
-	int len = 0;
-
-	while (h)
-	{
-		len++;
-		h = h->next;
-	}
-	return (len);
-}
-
-/**
- * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
- * @list: double pointer to the list to sort
+ * @list: list to be sorted
+ *
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = NULL, *one = NULL;
-	listint_t *two = NULL, *three = NULL, *four = NULL;
+	listint_t *forw, *tmp;
 
-	if (!list || !(*list) || len_list(*list) < 2)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	curr = *list;
-
-	while (curr)
+	for (forw = (*list)->next; forw && forw->prev; forw = forw->next)
 	{
-		if (curr->prev && curr->n < curr->prev->n)
+		for (; forw && forw->prev && forw->n < forw->prev->n;
+		     forw = forw->prev)
 		{
-			one = curr->prev->prev;
-			two = curr->prev;
-			three = curr;
-			four = curr->next;
-
-			two->next = four;
-			if (four)
-				four->prev = two;
-			three->next = two;
-			three->prev = one;
-			if (one)
-				one->next = three;
-			else
-				*list = three;
-			two->prev = three;
-			curr = *list;
+			tmp = forw->prev;
+			swap(list, tmp, forw);
 			print_list(*list);
-			continue;
+			forw = forw->next;
 		}
-		else
-			curr = curr->next;
 	}
+}
+
+/**
+ * swap - swaps two nodes
+ * @head: the head node
+ * @node1: The first node
+ * @node2: the second node
+ *
+ * Return: void
+ */
+void swap(listint_t **head, listint_t *node1, listint_t *node2)
+{
+	listint_t *prev, *next;
+
+	prev = node1->prev;
+	next = node2->next;
+
+	if (prev != NULL)
+		prev->next = node2;
+	else
+		*head = node2;
+
+	node1->prev = node2;
+	node1->next = next;
+	node2->prev = prev;
+	node2->next = node1;
+	if (next)
+		next->prev = node1;
 }
